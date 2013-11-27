81de3993ce584ae5a80da1415f905c4d53dafe12
dalayalmeida <dalay.almeida@hotmail.com>
2013-11-27 21:24:37 -0200

    acrescentando testes

--- test/NewEmptyJUnitTest.java
+++ test/NewEmptyJUnitTest.java
@@ -51,12 +51,52 @@ public class NewEmptyJUnitTest {
         assertEquals("tópico", sb.getTopic());
     }
     
-//    @Test
-//    public void testSubscriber(){
-//        s.subscribe();
-//        s.unsubscribe();
-//        s.tratarMensagem("tópico");
-//    }
+    @Test
+    public void testGui(){
+        sec.exibirInformacao("1;pollutionMonitor;TRAFEGO_ON");
+        sec.exibirInformacao("2;pollutionMonitor;TRAFEGO_ON");
+        sec.exibirInformacao("3;pollutionMonitor;TRAFEGO_ON");
+        sec.exibirInformacao("4;pollutionMonitor;TRAFEGO_ON");
+        sec.exibirInformacao("1;pollutionMonitor;TRAFEGO_OFF");
+        sec.exibirInformacao("2;pollutionMonitor;TRAFEGO_OFF");
+        sec.exibirInformacao("3;pollutionMonitor;TRAFEGO_OFF");
+        sec.exibirInformacao("4;pollutionMonitor;TRAFEGO_OFF");
+        
+        sec.exibirInformacao("1;willRainMonitor;RAIN_ON");
+        sec.exibirInformacao("2;willRainMonitor;RAIN_ON");
+        sec.exibirInformacao("3;willRainMonitor;RAIN_ON");
+        sec.exibirInformacao("4;willRainMonitor;RAIN_ON");
+        sec.exibirInformacao("1;willRainMonitor;RAIN_OFF");
+        sec.exibirInformacao("2;willRainMonitor;RAIN_OFF");
+        sec.exibirInformacao("3;willRainMonitor;RAIN_OFF");
+        sec.exibirInformacao("4;willRainMonitor;RAIN_OFF");
+        
+        
+        sec.exibirInformacao("1;acidRainMonitor;AQUI;teste");
+        sec.exibirInformacao("2;acidRainMonitor;AQUI;teste");
+        sec.exibirInformacao("3;acidRainMonitor;AQUI;teste");
+        sec.exibirInformacao("4;acidRainMonitor;AQUI;teste");
+        sec.exibirInformacao("1;acidRainMonitor;OUTRO;teste");
+        sec.exibirInformacao("2;acidRainMonitor;OUTRO;teste");
+        sec.exibirInformacao("3;acidRainMonitor;OUTRO;teste");
+        sec.exibirInformacao("4;acidRainMonitor;OUTRO;teste");
+        sec.exibirInformacao("1;acidRainMonitor;ACID_OFF;teste");
+        sec.exibirInformacao("2;acidRainMonitor;ACID_OFF;teste");
+        sec.exibirInformacao("3;acidRainMonitor;ACID_OFF;teste");
+        sec.exibirInformacao("4;acidRainMonitor;ACID_OFF;teste");
+        
+        
+        sec.exibirInformacao("1;noiseMonitor;NOISE_ON");
+        sec.exibirInformacao("2;noiseMonitor;NOISE_ON");
+        sec.exibirInformacao("3;noiseMonitor;NOISE_ON");
+        sec.exibirInformacao("4;noiseMonitor;NOISE_ON");
+        sec.exibirInformacao("1;noiseMonitor;NOISE_OFF");
+        sec.exibirInformacao("2;noiseMonitor;NOISE_OFF");
+        sec.exibirInformacao("3;noiseMonitor;NOISE_OFF");
+        sec.exibirInformacao("4;noiseMonitor;NOISE_OFF");
+        
+        
+    }
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
