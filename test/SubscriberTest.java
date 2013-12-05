92adb97b2d81b6693a0d438e6b6257f61a8bfda6
dalayalmeida <dalay.almeida@hotmail.com>
2013-12-05 00:35:17 -0300

    Testes da GUI

--- test/SubscriberTest.java
+++ test/SubscriberTest.java
@@ -19,86 +19,92 @@ import static org.junit.Assert.*;
  * @author felipe
  */
 public class SubscriberTest {
+
     SubscribeBean sb = new SubscribeBean();
     SecretariaMeioAmbiente sec = new SecretariaMeioAmbiente();
     Subscriber s = new AtualizaSubscriber("topico", sec);
-    
+
     public SubscriberTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-    
+
     @Test
-    public void testSubscribeBean(){
+    public void testSubscribeBean() {
         sb.setPort(11);
         assertEquals(11, sb.getPort());
         sb.setAndress("endereco");
         assertEquals("endereco", sb.getAndress());
         sb.setTopic("topico");
         assertEquals("topico", sb.getTopic());
-        
+
     }
-    
+
     @Test
-    public void testGui(){
-        sec.exibirInformacao("1;pollutionMonitor;TRAFEGO_ON");
-        sec.exibirInformacao("2;pollutionMonitor;TRAFEGO_ON");
-        sec.exibirInformacao("3;pollutionMonitor;TRAFEGO_ON");
-        sec.exibirInformacao("4;pollutionMonitor;TRAFEGO_ON");
-        sec.exibirInformacao("1;pollutionMonitor;TRAFEGO_OFF");
-        sec.exibirInformacao("2;pollutionMonitor;TRAFEGO_OFF");
-        sec.exibirInformacao("3;pollutionMonitor;TRAFEGO_OFF");
-        sec.exibirInformacao("4;pollutionMonitor;TRAFEGO_OFF");
-        
-        sec.exibirInformacao("1;willRainMonitor;RAIN_ON");
-        sec.exibirInformacao("2;willRainMonitor;RAIN_ON");
-        sec.exibirInformacao("3;willRainMonitor;RAIN_ON");
-        sec.exibirInformacao("4;willRainMonitor;RAIN_ON");
-        sec.exibirInformacao("1;willRainMonitor;RAIN_OFF");
-        sec.exibirInformacao("2;willRainMonitor;RAIN_OFF");
-        sec.exibirInformacao("3;willRainMonitor;RAIN_OFF");
-        sec.exibirInformacao("4;willRainMonitor;RAIN_OFF");
-        
-        
-        sec.exibirInformacao("1;acidRainMonitor;AQUI;teste");
-        sec.exibirInformacao("2;acidRainMonitor;AQUI;teste");
-        sec.exibirInformacao("3;acidRainMonitor;AQUI;teste");
-        sec.exibirInformacao("4;acidRainMonitor;AQUI;teste");
-        sec.exibirInformacao("1;acidRainMonitor;OUTRO;teste");
-        sec.exibirInformacao("2;acidRainMonitor;OUTRO;teste");
-        sec.exibirInformacao("3;acidRainMonitor;OUTRO;teste");
-        sec.exibirInformacao("4;acidRainMonitor;OUTRO;teste");
-        sec.exibirInformacao("1;acidRainMonitor;ACID_OFF;teste");
-        sec.exibirInformacao("2;acidRainMonitor;ACID_OFF;teste");
-        sec.exibirInformacao("3;acidRainMonitor;ACID_OFF;teste");
-        sec.exibirInformacao("4;acidRainMonitor;ACID_OFF;teste");
-        
-        
-        sec.exibirInformacao("1;noiseMonitor;NOISE_ON");
-        sec.exibirInformacao("2;noiseMonitor;NOISE_ON");
-        sec.exibirInformacao("3;noiseMonitor;NOISE_ON");
-        sec.exibirInformacao("4;noiseMonitor;NOISE_ON");
-        sec.exibirInformacao("1;noiseMonitor;NOISE_OFF");
-        sec.exibirInformacao("2;noiseMonitor;NOISE_OFF");
-        sec.exibirInformacao("3;noiseMonitor;NOISE_OFF");
-        sec.exibirInformacao("4;noiseMonitor;NOISE_OFF");
-        
-        
-        
+    public void testGui() {
+        boolean passou = true;
+        try {
+            sec.exibirInformacao("1;pollutionMonitor;TRAFEGO_ON");
+            sec.exibirInformacao("2;pollutionMonitor;TRAFEGO_ON");
+            sec.exibirInformacao("3;pollutionMonitor;TRAFEGO_ON");
+            sec.exibirInformacao("4;pollutionMonitor;TRAFEGO_ON");
+            sec.exibirInformacao("1;pollutionMonitor;TRAFEGO_OFF");
+            sec.exibirInformacao("2;pollutionMonitor;TRAFEGO_OFF");
+            sec.exibirInformacao("3;pollutionMonitor;TRAFEGO_OFF");
+            sec.exibirInformacao("4;pollutionMonitor;TRAFEGO_OFF");
+
+            sec.exibirInformacao("1;willRainMonitor;RAIN_ON");
+            sec.exibirInformacao("2;willRainMonitor;RAIN_ON");
+            sec.exibirInformacao("3;willRainMonitor;RAIN_ON");
+            sec.exibirInformacao("4;willRainMonitor;RAIN_ON");
+            sec.exibirInformacao("1;willRainMonitor;RAIN_OFF");
+            sec.exibirInformacao("2;willRainMonitor;RAIN_OFF");
+            sec.exibirInformacao("3;willRainMonitor;RAIN_OFF");
+            sec.exibirInformacao("4;willRainMonitor;RAIN_OFF");
+
+
+            sec.exibirInformacao("1;acidRainMonitor;AQUI;teste");
+            sec.exibirInformacao("2;acidRainMonitor;AQUI;teste");
+            sec.exibirInformacao("3;acidRainMonitor;AQUI;teste");
+            sec.exibirInformacao("4;acidRainMonitor;AQUI;teste");
+            sec.exibirInformacao("1;acidRainMonitor;OUTRO;teste");
+            sec.exibirInformacao("2;acidRainMonitor;OUTRO;teste");
+            sec.exibirInformacao("3;acidRainMonitor;OUTRO;teste");
+            sec.exibirInformacao("4;acidRainMonitor;OUTRO;teste");
+            sec.exibirInformacao("1;acidRainMonitor;ACID_OFF;teste");
+            sec.exibirInformacao("2;acidRainMonitor;ACID_OFF;teste");
+            sec.exibirInformacao("3;acidRainMonitor;ACID_OFF;teste");
+            sec.exibirInformacao("4;acidRainMonitor;ACID_OFF;teste");
+
+
+            sec.exibirInformacao("1;noiseMonitor;NOISE_ON");
+            sec.exibirInformacao("2;noiseMonitor;NOISE_ON");
+            sec.exibirInformacao("3;noiseMonitor;NOISE_ON");
+            sec.exibirInformacao("4;noiseMonitor;NOISE_ON");
+            sec.exibirInformacao("1;noiseMonitor;NOISE_OFF");
+            sec.exibirInformacao("2;noiseMonitor;NOISE_OFF");
+            sec.exibirInformacao("3;noiseMonitor;NOISE_OFF");
+            sec.exibirInformacao("4;noiseMonitor;NOISE_OFF");
+        } catch (NullPointerException e) {
+            passou = false;
+        }
+
+        assertTrue(passou);
+
     }
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
