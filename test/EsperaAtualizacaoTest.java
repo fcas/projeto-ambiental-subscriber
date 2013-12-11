d8bfb1c86741c878d7a5f87f7114983b43e0e78d
dalayalmeida <dalay.almeida@hotmail.com>
2013-12-11 20:17:44 -0300

    teste sem erros

--- test/EsperaAtualizacaoTest.java
+++ test/EsperaAtualizacaoTest.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 
+import br.ufrn.GUI.SecretariaMeioAmbiente;
 import br.ufrn.subscriber.EsperarAtualizacao;
 import br.ufrn.subscriber.TratarMensagem;
 import java.io.IOException;
@@ -22,9 +23,11 @@ import static org.junit.Assert.*;
  */
 public class EsperaAtualizacaoTest {
     
-    public TratarMensagem msg;
-    public EsperarAtualizacao ea; 
-    public Socket socket;
+//    public TratarMensagem msg;
+//    public EsperarAtualizacao ea; 
+//    public Socket socket;
+    
+    SecretariaMeioAmbiente sec = new SecretariaMeioAmbiente();
     
     
     public EsperaAtualizacaoTest() {
@@ -41,18 +44,18 @@ public class EsperaAtualizacaoTest {
     
     @Before
     public void setUp() throws UnknownHostException, IOException {
-        msg = new TratarMensagem() {
-
-            @Override
-            public void tratarMensagem(String mensagem) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-            }
-        };
-        ea = new EsperarAtualizacao(9000, msg);
-        try {
-          socket = new Socket("localhost", 8080);
-        } catch (Exception e) {
-        }
+//        msg = new TratarMensagem() {
+//
+//            @Override
+//            public void tratarMensagem(String mensagem) {
+//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//            }
+//        };
+//        ea = new EsperarAtualizacao(9000, msg);
+//        try {
+//          socket = new Socket("localhost", 8080);
+//        } catch (Exception e) {
+//        }
 
     }
     
@@ -67,27 +70,116 @@ public class EsperaAtualizacaoTest {
     
     @Test
     public void testExtrairMsgFail() throws IOException{
-      boolean thrown = false;
-      
-        try {
-                ea.extrairMensagem(socket); 
-        } catch (NullPointerException e) {
-            thrown = true;
-         }
-        
-        assertTrue(thrown);
+//      boolean thrown = false;
+//      
+//        try {
+//                ea.extrairMensagem(socket); 
+//        } catch (NullPointerException e) {
+//            thrown = true;
+//         }
+//        
+//        assertTrue(thrown);
     }
     
     @Test
     public void testRunFail() throws IOException{
-      boolean thrown = false;
-      
+//      boolean thrown = false;
+//      
+//        try {
+//                ea.run(); 
+//        } catch (NullPointerException e) {
+//            thrown = true;
+//         }
+//        
+//        assertTrue(thrown);
+    }
+    
+    @Test
+    public void testGui() {
+        boolean passou = true;
         try {
-                ea.run(); 
+            sec.exibirInformacao("1;pollutionMonitor;TRAFEGO_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("2;pollutionMonitor;TRAFEGO_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("3;pollutionMonitor;TRAFEGO_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("4;pollutionMonitor;TRAFEGO_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("1;pollutionMonitor;TRAFEGO_OFF");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("2;pollutionMonitor;TRAFEGO_OFF");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("3;pollutionMonitor;TRAFEGO_OFF");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("4;pollutionMonitor;TRAFEGO_OFF");
+            assertTrue(sec.pegarTexto()!="");
+
+            sec.exibirInformacao("1;willRainMonitor;RAIN_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("2;willRainMonitor;RAIN_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("3;willRainMonitor;RAIN_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("4;willRainMonitor;RAIN_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("1;willRainMonitor;RAIN_OFF");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("2;willRainMonitor;RAIN_OFF");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("3;willRainMonitor;RAIN_OFF");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("4;willRainMonitor;RAIN_OFF");
+            assertTrue(sec.pegarTexto()!="");
+
+
+            sec.exibirInformacao("1;acidRainMonitor;AQUI;teste");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("2;acidRainMonitor;AQUI;teste");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("3;acidRainMonitor;AQUI;teste");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("4;acidRainMonitor;AQUI;teste");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("1;acidRainMonitor;OUTRO;teste");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("2;acidRainMonitor;OUTRO;teste");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("3;acidRainMonitor;OUTRO;teste");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("4;acidRainMonitor;OUTRO;teste");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("1;acidRainMonitor;ACID_OFF;teste");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("2;acidRainMonitor;ACID_OFF;teste");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("3;acidRainMonitor;ACID_OFF;teste");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("4;acidRainMonitor;ACID_OFF;teste");
+            assertTrue(sec.pegarTexto()!="");
+
+
+            sec.exibirInformacao("1;noiseMonitor;NOISE_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("2;noiseMonitor;NOISE_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("3;noiseMonitor;NOISE_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("4;noiseMonitor;NOISE_ON");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("1;noiseMonitor;NOISE_OFF");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("2;noiseMonitor;NOISE_OFF");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("3;noiseMonitor;NOISE_OFF");
+            assertTrue(sec.pegarTexto()!="");
+            sec.exibirInformacao("4;noiseMonitor;NOISE_OFF");
+            assertTrue(sec.pegarTexto()!="");
         } catch (NullPointerException e) {
-            thrown = true;
-         }
-        
-        assertTrue(thrown);
+            passou = false;
+        }
+
+        assertTrue(passou);
+
     }
 }
