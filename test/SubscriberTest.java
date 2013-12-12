b52b2215c805d613794190b55f0ce8c4df81cbf8
dalayalmeida <dalay.almeida@hotmail.com>
2013-12-12 01:20:42 -0300

    adicionamento de testes

--- test/SubscriberTest.java
+++ test/SubscriberTest.java
@@ -19,42 +19,142 @@ import static org.junit.Assert.*;
  * @author felipe
  */
 public class SubscriberTest {
+
     SubscribeBean sb;
     SecretariaMeioAmbiente sec;
-    
+
     public SubscriberTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
         sb = new SubscribeBean();
         sec = new SecretariaMeioAmbiente();
     }
-    
+
     @After
     public void tearDown() {
     }
 //    
+
     @Test
-    public void testSubscribeBean(){
+    public void testSubscribeBean() {
         sb.setPort(11);
         assertEquals(11, sb.getPort());
         sb.setAndress("endereco");
         assertEquals("endereco", sb.getAndress());
         sb.setTopic("topico");
         assertEquals("topico", sb.getTopic());
-        
-    }
-    
 
+    }
+
+    @Test
+    public void testGuiChuva() {
+        sec.limpaTexto();
+        sec.exibirInformacao("1;willRainMonitor;RAIN_ON");
+        assertEquals(sec.pegarTexto(), "Chuva na área 1\n");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("2;willRainMonitor;RAIN_ON");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("3;willRainMonitor;RAIN_ON");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("4;willRainMonitor;RAIN_ON");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("1;willRainMonitor;RAIN_OFF");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("2;willRainMonitor;RAIN_OFF");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("3;willRainMonitor;RAIN_OFF");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("4;willRainMonitor;RAIN_OFF");
+//        assertTrue(!sec.pegarTexto().equals(""));
+    }
+
+//    @Test
+//    public void testGuiChuvaAcida() {
+//        sec.exibirInformacao("1;acidRainMonitor;AQUI;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("2;acidRainMonitor;AQUI;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("3;acidRainMonitor;AQUI;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("4;acidRainMonitor;AQUI;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("1;acidRainMonitor;OUTRO;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("2;acidRainMonitor;OUTRO;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("3;acidRainMonitor;OUTRO;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("4;acidRainMonitor;OUTRO;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("1;acidRainMonitor;ACID_OFF;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("2;acidRainMonitor;ACID_OFF;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("3;acidRainMonitor;ACID_OFF;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("4;acidRainMonitor;ACID_OFF;teste");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//    }
+//
+//    @Test
+//    public void testGuiBarulho() {
+//        sec.exibirInformacao("1;noiseMonitor;NOISE_ON");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("2;noiseMonitor;NOISE_ON");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("3;noiseMonitor;NOISE_ON");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("4;noiseMonitor;NOISE_ON");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("1;noiseMonitor;NOISE_OFF");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("2;noiseMonitor;NOISE_OFF");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("3;noiseMonitor;NOISE_OFF");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//        sec.exibirInformacao("4;noiseMonitor;NOISE_OFF");
+//        assertTrue(!sec.pegarTexto().equals(""));
+//    }
+//
+//    @Test
+//    public void testGuiPoluicao() {
+//        boolean passou = true;
+//        try {
+//            sec.exibirInformacao("1;pollutionMonitor;TRAFEGO_ON");
+//            assertTrue(!sec.pegarTexto().equals(""));
+//            sec.exibirInformacao("2;pollutionMonitor;TRAFEGO_ON");
+//            assertTrue(!sec.pegarTexto().equals(""));
+//            sec.exibirInformacao("3;pollutionMonitor;TRAFEGO_ON");
+//            assertTrue(!sec.pegarTexto().equals(""));
+//            sec.exibirInformacao("4;pollutionMonitor;TRAFEGO_ON");
+//            assertTrue(!sec.pegarTexto().equals(""));
+//            sec.exibirInformacao("1;pollutionMonitor;TRAFEGO_OFF");
+//            assertTrue(!sec.pegarTexto().equals(""));
+//            sec.exibirInformacao("2;pollutionMonitor;TRAFEGO_OFF");
+//            assertTrue(!sec.pegarTexto().equals(""));
+//            sec.exibirInformacao("3;pollutionMonitor;TRAFEGO_OFF");
+//            assertTrue(!sec.pegarTexto().equals(""));
+//            sec.exibirInformacao("4;pollutionMonitor;TRAFEGO_OFF");
+//            assertTrue(!sec.pegarTexto().equals(""));
+//
+//
+//        } catch (NullPointerException e) {
+//            passou = false;
+//        }
+//
+//        assertTrue(passou);
+//
+//    }
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
